188aa86a9e2cc4711b4d35d4fca6f208a7d91cc6
arbi.topi <topiarbi@gmail.com>
2025-03-25 12:08:27 +0100

    Nisja e krijimit te statistika aplikimesh

--- src/main/java/StatistikaPages/StatistikaAplikimeshPage.java
+++ src/main/java/StatistikaPages/StatistikaAplikimeshPage.java
@@ -2,10 +2,14 @@ package StatistikaPages;
 
 import StatistikaPagesElements.StatistikaAplikimeshElements;
 import Utilities.BaseInformation;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import javax.print.DocFlavor;
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
 public class StatistikaAplikimeshPage {
     StatistikaAplikimeshElements page = new StatistikaAplikimeshElements();
@@ -14,5 +18,174 @@ public class StatistikaAplikimeshPage {
 
     public void waitUntilNoTableReaload(){
         wait.until(ExpectedConditions.invisibilityOf(page.loaderTable));
+    }
+    public void selectTipi(String value){
+        wait.until(ExpectedConditions.elementToBeClickable(page.tipiDD));
+        page.tipiDD.click();
+        wait.until(ExpectedConditions.visibilityOf(page.tipiOptions.getFirst()));
+        for(WebElement item: page.tipiOptions){
+            String Text = item.getText();
+            if(Text.contains(value)){
+                item.click();
+                break;
+            }
+        }
+        waitUntilNoTableReaload();
+    }
+    public void selektoDaten(String dataFillimit,String dataMbarimit){
+        wait.until(ExpectedConditions.elementToBeClickable(page.nisjaEdatesTB));
+        page.nisjaEdatesTB.clear();
+        page.nisjaEdatesTB.sendKeys(dataFillimit);
+        page.fundiEdatesTB.clear();
+        page.fundiEdatesTB.sendKeys(dataMbarimit);
+    }
+    public void nrAplikimit(String value){
+        page.nrIAplikimit.clear();
+        page.nrIAplikimit.sendKeys(value);
+    }
+    public void selectMinistria(String value){
+        wait.until(ExpectedConditions.elementToBeClickable(page.ministriaDD));
+        page.ministriaDD.click();
+        wait.until(ExpectedConditions.visibilityOf(page.minsitriaOptions.getFirst()));
+        for(WebElement item: page.minsitriaOptions){
+            String Text = item.getText();
+            if(Text.contains(value)){
+                item.click();
+                break;
+            }
+        }
+        waitUntilNoTableReaload();
+    }
+    public void selectInstitucionet(String value){
+        wait.until(ExpectedConditions.elementToBeClickable(page.institucionetDD));
+        page.institucionetDD.click();
+        wait.until(ExpectedConditions.visibilityOf(page.institucionetOptions.getFirst()));
+        for(WebElement item: page.institucionetOptions){
+            String Text = item.getText();
+            if(Text.contains(value)){
+                item.click();
+                break;
+            }
+        }
+        waitUntilNoTableReaload();
+    }
+    public void selectInstitucioniPrites(String value){
+        wait.until(ExpectedConditions.elementToBeClickable(page.institucionetPriteseDD));
+        page.institucionetPriteseDD.click();
+        wait.until(ExpectedConditions.visibilityOf(page.institucionetOptions.getFirst()));
+        for(WebElement item: page.institucionetOptions){
+            String Text = item.getText();
+            if(Text.contains(value)){
+                item.click();
+                break;
+            }
+        }
+        waitUntilNoTableReaload();
+
+    }
+    public void selectSherbimtet(String value){
+        wait.until(ExpectedConditions.elementToBeClickable(page.sherbimetDD));
+        page.sherbimetDD.click();
+        wait.until(ExpectedConditions.visibilityOf(page.sherbimetOptions.getFirst()));
+        for(WebElement item: page.sherbimetOptions){
+            String Text = item.getText();
+            if(Text.contains(value)){
+                item.click();
+                break;
+            }
+        }
+        waitUntilNoTableReaload();
+    }
+    public void selectStatusi(String value){
+        wait.until(ExpectedConditions.elementToBeClickable(page.statusiDD));
+        page.statusiDD.click();
+        wait.until(ExpectedConditions.visibilityOf(page.statusiOptions.getFirst()));
+        for(WebElement item: page.statusiOptions){
+            String Text = item.getText();
+            if(Text.contains(value)){
+                item.click();
+                break;
+            }
+        }
+        waitUntilNoTableReaload();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    public List<String> statusiTable(){
+        waitUntilNoTableReaload();
+        List<String> list = new ArrayList<>();
+        for (WebElement item: page.statusiTable){
+            String text = item.getText();
+            list.add(text);
+        }
+        return list;
+    }
+    public List<String> sherbimiTable(){
+        waitUntilNoTableReaload();
+        List<String> list = new ArrayList<>();
+        for (WebElement item: page.sherbimetTable){
+            String text = item.getText();
+            list.add(text);
+        }
+        return list;
+    }
+    public List<String> institucioniTable(){
+        waitUntilNoTableReaload();
+        List<String> list = new ArrayList<>();
+        for (WebElement item: page.institucionetTable){
+            String text = item.getText();
+            list.add(text);
+        }
+        return list;
+    }
+
+    public boolean isTableEmpty(){
+        waitUntilNoTableReaload();
+        return page.emptyTable.isDisplayed();
+    }
+    public void mbyllFiltrat(){
+        wait.until(ExpectedConditions.elementToBeClickable(page.mbyllFiltrat));
+        page.mbyllFiltrat.click();
+    }
+    public void hapFiltrat(){
+        wait.until(ExpectedConditions.elementToBeClickable(page.nxirrFiltrat));
+        page.nxirrFiltrat.click();
+    }
+    public void pastroFiltrat(){
+        wait.until(ExpectedConditions.elementToBeClickable(page.pastroFiltrat));
+        page.pastroFiltrat.click();
+
+    }
+    public int tableSize(){
+        return page.tableSize.size();
+    }
+
+
+
+
+    public String getNrAplikimitTable(){
+    return      page.nrAplikimitTABLE.getText();
+    }
+    public String getSherbimiTable(){
+        return page.sherbimiTABLE.getText();
+    }
+    public String getInstitucionitTABLE(){
+        return page.institucioniTABLE.getText();
+    }
+    public String getNiptID(){
+        return page.niptTABLE.getText();
+    }
+    public String getStatusTABLE(){
+        return page.statusiTABLE.getText();
     }
 }
--- src/main/java/StatistikaPagesElements/StatistikaAplikimeshElements.java
+++ src/main/java/StatistikaPagesElements/StatistikaAplikimeshElements.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
+
 public class StatistikaAplikimeshElements {
     public StatistikaAplikimeshElements(){
         PageFactory.initElements(BaseInformation.getDriver(),this);
@@ -12,7 +14,9 @@ public class StatistikaAplikimeshElements {
     @FindBy(css = "#applications_table_processing")
     public WebElement loaderTable;
     @FindBy(css = "#select2-llojiAplikimit-container")
-    public WebElement tipiFilter;
+    public WebElement tipiDD;
+    @FindBy(xpath = "//span/ul/li[@class='select2-results__option']")
+    public List<WebElement> tipiOptions;
     @FindBy(css = "#startDatePicker")
     public WebElement nisjaEdatesTB;
     @FindBy(css = "#endDatePicker")
@@ -21,10 +25,54 @@ public class StatistikaAplikimeshElements {
     public WebElement nrIAplikimit;
     @FindBy(css = "#select2-Ministry-container")
     public WebElement ministriaDD;
+    @FindBy(xpath = "//span/ul/li[@class='select2-results__option']")
+    public List<WebElement> minsitriaOptions;
+    @FindBy(xpath = "/html/body/div[1]/div[3]/div/div[2]/aside/div/div/div/div/div[6]/span/span[1]/span/ul/li/input")
+    public WebElement institucionetDD;
+    @FindBy(xpath = "//ul[@id='select2-Institution-results']/li/ul/li/span")
+    public List<WebElement> institucionetOptions;
+    @FindBy(xpath = "//ul[@id='select2-Services-results']/li")
+    public List<WebElement> sherbimetOptions;
+    @FindBy(xpath = "/html/body/div[1]/div[3]/div/div[2]/aside/div/div/div/div/div[7]/span/span[1]/span/ul/li/input" )
+    public WebElement institucionetPriteseDD;
+    @FindBy(xpath = "/html/body/div[1]/div[3]/div/div[2]/aside/div/div/div/div/div[8]/span/span[1]/span/ul/li/input")
+    public WebElement sherbimetDD;
+    @FindBy(css = "/html/body/div[1]/div[3]/div/div[2]/aside/div/div/div/div/div[9]/span/span[1]/span/ul/li/input")
+    public WebElement statusiDD;
+    @FindBy(xpath = "//ul[@id='select2-statusi-results']/li")
+    public List<WebElement> statusiOptions;
+
+    //filtrim buttons
     @FindBy(css = ".btn.btn-sm.bg-white.rounded-circle.btn-icon.text-primary")
     public WebElement mbyllFiltrat;
     @FindBy(css = "#showFiltersBtn")
     public WebElement nxirrFiltrat;
     @FindBy(css = "#clearFilters")
     public WebElement pastroFiltrat;
+    @FindBy(css = "select[name='applications_table_length']")
+    public WebElement shfaqTableData;
+    //table
+    @FindBy(xpath = "//tbody/tr/td[2]")
+    public List<WebElement> tableSize;
+    @FindBy(css = "#clearFilters")
+    public WebElement emptyTable;
+    @FindBy(xpath = "//tbody/tr/td[8]")
+    public List<WebElement>  statusiTable;
+    @FindBy(xpath = "//tbody/tr/td[4]")
+    public List<WebElement> sherbimetTable;
+    @FindBy(xpath = "//tbody/tr/td[5]")
+    public List<WebElement> institucionetTable;
+    //first table data
+    @FindBy(xpath = "//tbody/tr[1]/td[3]")
+    public WebElement nrAplikimitTABLE;
+    @FindBy(xpath = "//tbody/tr[1]/td[4]")
+    public WebElement sherbimiTABLE;
+    @FindBy(xpath = "//tbody/tr[1]/td[5]")
+    public WebElement institucioniTABLE;
+    @FindBy(xpath = "//tbody/tr[1]/td[7]")
+    public WebElement niptTABLE;
+    @FindBy(xpath = "//tbody/tr[1]/td[8]")
+    public WebElement statusiTABLE;
+
+
 }
