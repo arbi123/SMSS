23c200830163cba72d1aa9f7e526e32eeb3778e2
arbi.topi <topiarbi@gmail.com>
2025-03-26 17:07:55 +0100

    Nisja e krijimit te statistika aplikimesh

--- src/main/java/StatistikaPagesElements/StatistikaNderInstitucionElements.java
+++ src/main/java/StatistikaPagesElements/StatistikaNderInstitucionElements.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
+
 public class StatistikaNderInstitucionElements {
 
     public StatistikaNderInstitucionElements(){
@@ -16,4 +18,30 @@ public class StatistikaNderInstitucionElements {
     public WebElement loaderTable;
     @FindBy(xpath = "//div[@class='loader']")
     public WebElement divLoader;
+    @FindBy(css = "#nrAplikimi")
+    public WebElement nrIAplikimit;
+    @FindBy(css = "#startDatePicker")
+    public WebElement nisjaEdatesTB;
+    @FindBy(css = "#endDatePicker")
+    public WebElement fundiEdatesTB;
+    @FindBy(css = "#select2-Ministry-container")
+    public WebElement ministriaDergueseDD;
+    @FindBy(xpath = "//span/ul/li[@class='select2-results__option']")
+    public List<WebElement> minsitriaOptions;
+    @FindBy(xpath = "//select[@data-select2-id='Institution']/following-sibling::span/span/span/ul/li/input")
+    public WebElement institucionetDergueseDD;
+    @FindBy(xpath = "//span/ul/li[@class='select2-results__option']/ul/li/span")
+    public WebElement institucionetDergueseOptions;
+    @FindBy(css = "#select2-MinistryReceiver-container")
+    public WebElement ministriaPriteseDD;
+    @FindBy(css = "#select2-ResponseBack-container")
+    public WebElement selectKthimPergjigje;
+    @FindBy(css = "#insideDeadline")
+    public WebElement brendaAfatitRB;
+    @FindBy(css = "#outsideDeadline")
+    public WebElement jashtAfatitRB;
+    @FindBy(css = "#clearFilters")
+    public WebElement pastroFiltratButton;
+
+
 }
