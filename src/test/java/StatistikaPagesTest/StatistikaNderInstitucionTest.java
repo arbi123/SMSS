e934ccc9d22eb6f3f1f23248a73efe2fa86975e7
arbi.topi <topiarbi@gmail.com>
2025-03-27 13:02:02 +0100

    Nisja e krijimit te statistika aplikimesh

--- src/test/java/StatistikaPagesTest/StatistikaNderInstitucionTest.java
+++ src/test/java/StatistikaPagesTest/StatistikaNderInstitucionTest.java
@@ -1,16 +1,21 @@
 package StatistikaPagesTest;
 
+import ConfigPagesTest.AssertInfo;
+import DataProviders.MinistriteFilterDP;
 import Globals.Globals;
 import StatistikaPages.StatistikaAplikimeshPage;
 import StatistikaPages.StatistikaNderInstitucionPage;
 import Utilities.BaseInformation;
+import org.apache.poi.ss.formula.functions.T;
 import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import java.time.Duration;
 
@@ -31,14 +36,146 @@ public class StatistikaNderInstitucionTest {
         driver.navigate().refresh();
     }
     @Test(priority = 0)
-    public void navigoTekFaqja(){
+    public void navigoTekFaqja() throws InterruptedException {
         driver.navigate().to(Globals.statistikaNderInstitucionURL);
         wait.until(ExpectedConditions.urlToBe(Globals.statistikaNderInstitucionURL));
         Assert.assertEquals(driver.getCurrentUrl(),Globals.statistikaNderInstitucionURL);
         page.waitUntilNoTableReaload();
     }
-    @Test
-    public void quit(){
+//    @Test(priority = 1)
+//    public void zgjidhMinistrindheInstitucioniDergues() throws InterruptedException {
+//        page.selektoDaten("21/01/2025","23/03/2025");
+//        page.selectMinistriaDerguese("Ministria e Drejtësisë");
+//        page.selectInstitucioniDergues("Dhoma Kombëtare e Noterëve");
+//        page.selektoBrendaAfatit();
+//
+//
+//    }
+//    @Test(priority = 2)
+//    public void selectMinsitriaDheInstitucionetPrites(){
+//        page.selectMinistriaPritese("Ministria e Brendshme");
+//        page.selectInstitucionePritese("Drejtoria e Përgjithshme e Gjendjes Civile (DPGJC)");
+//        page.selectKthimPergjigje("Po");
+//        page.selektoJashtAfatit();
+//    }
+    @Test(priority = 3)
+    @AssertInfo({
+            "Testimi i funksionalitetit të zgjedhjes së institucionit dërgues",
+            "Verifikimi i saktësisë së filtrit të ministrisë dërguese",
+            "Konfirmimi që të gjitha rezultatet përputhen me institucionin e zgjedhur",
+            "Kontrolli i përputhjes së çdo elementi në tabelë me institucionin dërgues",
+            "Sigurimi i saktë i rezultateve pas përzgjedhjes dhe shfaqjes së të dhënave",
+            "Vlerësimi gjithëpërfshirës i filtrimit të institucioneve"
+    })
+    public void selektimiDheVerifikimiIinstitucionitDergues(){
+        String ministriaDerguese="Ministria e Drejtësisë";
+        String institucioniDergues="Dhoma Kombëtare e Noterëve";
+        page.selectMinistriaDerguese(ministriaDerguese);
+        page.selectInstitucioniDergues(institucioniDergues);
+        page.shfaqeTeDhena("100");
+        for (String item: page.institucioniDergues()){
+            Assert.assertEquals(item,institucioniDergues,"Nuk jan te njejte institucionet");
+        }
+    }
+    @Test(priority = 4)
+    @AssertInfo({
+            "Testimi i funksionalitetit të filtrimit me kthim përgjigje",
+            "Verifikimi i saktësisë së filtrit të kthimit të përgjigjes",
+            "Konfirmimi që të gjitha rezultatet përputhen me statusin specifik",
+            "Kontrolli i përputhjes së çdo elementi në tabelë me statusin e shërbimit",
+            "Sigurimi i saktë i lidhjeve midis kthimit të përgjigjes dhe statusit",
+            "Vlerësimi gjithëpërfshirës i filtrimit dhe statuseve përkatëse"
+    })
+    public void kthimPergjigjePoDheVerifikim(){
+        String kthimPergjigje="Po";
+        page.selectKthimPergjigje(kthimPergjigje);
+        for (String item: page.statusi()){
+            Assert.assertEquals(item,"Lëvuar Shërbimi","Nuk jan te njejte statuset");
+        }
+    }
+    @Test(priority = 5,dataProvider = "ministriaTypes",dataProviderClass = MinistriteFilterDP.class)
+    @AssertInfo({
+            "Testimi i funksionalitetit të zgjedhjes së ministrisë dërguese",
+            "Verifikimi i saktësisë së institucioneve pas përzgjedhjes së ministrisë",
+            "Konfirmimi që institucioni dërgues përputhet me ministrinë e zgjedhur",
+            "Kontrolli i ndërveprimit midis ministrisë dhe institucioneve përkatëse",
+            "Sigurimi i saktë i filtrit të institucioneve bazuar në ministrinë",
+            "Vlerësimi i mekanizmit të përditësimit të institucioneve pas përzgjedhjes"
+    })
+    public void selectMinistriaDergueseVerifyInstitucionetOptions(String ministri){
+
+        page.selectMinistriaDerguese(ministri);
+        Assert.assertEquals(page.strongInstitucionDergusText(),ministri,"Kur selektuam ministrin perkatese nuk na doli institucionet te cilat i perkasin asaj minstrie");
+    }
+    @Test(priority = 6,dataProvider = "ministriaTypes",dataProviderClass = MinistriteFilterDP.class)
+    @AssertInfo({
+            "Testimi i funksionalitetit të zgjedhjes së ministrisë dërguese",
+            "Verifikimi i saktësisë së institucioneve pas përzgjedhjes së ministrisë",
+            "Konfirmimi që institucioni dërgues përputhet me ministrinë e zgjedhur",
+            "Kontrolli i ndërveprimit midis ministrisë dhe institucioneve përkatëse",
+            "Sigurimi i saktë i filtrit të institucioneve bazuar në ministrinë",
+            "Vlerësimi i mekanizmit të përditësimit të institucioneve pas përzgjedhjes"
+    })
+    public void selectMinistriaPriteseVerifyInstitucionetOptions(String ministri){
+        page.pastroFiltrat();
+        page.selectMinistriaPritese(ministri);
+        Assert.assertEquals(page.strongInstitucionPritesText(),ministri);
+    }
+    @Test(priority = 7)
+    @AssertInfo({
+            "Testimi i funksionalitetit të kërkimit me numër kërkese",
+            "Verifikimi i saktësisë së të gjitha detajeve pas kërkimit",
+            "Konfirmimi i përputhjes së numrit të kërkesës",
+            "Kontrolli i institucionit dërgues dhe pritës",
+            "Sigurimi i saktë i datës së kërkesës",
+            "Vlerësimi gjithëpërfshirës i detajeve të kërkesës specifike",
+            "Kontrollimi i statusit dhe përgjegjësit të kërkesës"
+    })
+    public void kerkimiDheVerifikimiMeNrKerkese() throws InterruptedException {
+        SoftAssert softAssert = new SoftAssert();
+        String nrKerkeses="SQDNE280220250036";
+        page.pastroFiltrat();
+        page.nrKerkeses(nrKerkeses);
+        softAssert.assertEquals(page.getNrKerkeses(),nrKerkeses,"");
+        softAssert.assertEquals(page.getInstitucionDergues(),"Instituti i Sigurimeve Shoqërore (ISSH)");
+        softAssert.assertTrue(page.getInstitucionPrites().contains("- Agjencia Kombëtare e Mjedisit"),"");
+        softAssert.assertTrue(page.getDataKerkeses().contains("- 28/02/2025"),"");
+        softAssert.assertTrue(page.getKerkesa().contains("- AMBU"),"");
+        softAssert.assertEquals(page.getPergjegjesi(),"Valbona Koci");
+        softAssert.assertEquals(page.getKthimPergjigje(),"28/02/2025");
+        softAssert.assertEquals(page.getStatusi(),"Aplikuar për dokumentacion shoqërues");
+        softAssert.assertAll();
+    }
+
+   @Test(priority = 7)
+   @AssertInfo({
+           "Testimi i dukshmërisë së opsioneve të filtrave",
+           "Verifikimi i qëndrueshmërisë së strukturës së filtrave",
+           "Konfirmimi i ruajtjes së opsioneve ekzistuese",
+           "Kontrolli i ndryshimeve në opsionet e filtrave",
+           "Sigurimi i saktë i qëndrueshmërisë së elementeve të filtrave",
+           "Vlerësimi i mundshëm i modifikimeve në funksionalitetin e filtrave"
+   })
+   public void vizibilitetiIOpsioneveSeFiltrave() throws InterruptedException {
+        page.clickTeTabelaButton();
+        Thread.sleep(1500);
+        Assert.fail("Jane hequr disa filtra dhe eshte shtuar nje tek Opsionet E Filtrave");
+   }
+   @Test(priority = 8)
+   public void filtrimiMeDate() throws InterruptedException {
+       page.shfaqeTeDhena("10");
+       page.clickTeTabelaButton();
+       page.pastroFiltrat();
+       page.selektoDaten("24/02/2025","26/02/2025");
+       page.clickLastPage();
+       Thread.sleep(5000);
+       page.getAllDataKerkesesNderInstit();
+       page.getAfatiKthimPergjigje();
+   }
+
+    @AfterClass
+    public void quit() throws InterruptedException {
+        Thread.sleep(4000);
         driver.quit();
     }
 
